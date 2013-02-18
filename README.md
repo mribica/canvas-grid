ed625d75018499aa2c68382efc27ad45dfa6c7b2
Muamer Ribica <muamer.ribica@gmail.com>
2013-02-18 20:54:24 +0100

    Init commit

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-canvas-grid
-===========
+# canvas-grid
 
-generate grid background for your canvas, optionally show cursor coordinates on mouse move.
+Generate grid background for your canvas, optionally show cursor coordinates on mouse move.
+
+# Usage
+
+## Basic
+
+~~~javascript
+var canvas = document.getElementById('canvas');
+canvas.show_grid();
+~~~
+
+## Customize
+
+### Options
+
+**color** (default #eee)
+Color for grid lines.
+
+**distance** (default 10)
+Distance between grid lines.
+
+**info_box_id** (default null)
+Show cursor coordinates in element with provided id.
+
+~~~javascript
+var canvas = document.getElementById('canvas');
+canvas.show_grid({
+  color: "#333", // default: #eee
+  distanace: 5,  // default 10px
+  info_box_id: "info_box"
+});
+~~~
---
+++ grid.js
@@ -0,0 +1,81 @@
+/*
+ * options = {
+ *  color: "#eee"
+ *  distance: 10
+ *  info_box: null
+ * }
+ */
+
+Object.extend = function(destination, source) {
+  for (var property in source) {
+    if (source.hasOwnProperty(property)) {
+      destination[property] = source[property];
+    }
+  }
+  return destination;
+};
+
+if (HTMLCanvasElement.prototype.grid == null) {
+  HTMLCanvasElement.prototype.show_grid = function (options) {
+    /*
+     * Default grid options
+     */
+    var defaults = {
+      color: "#eee",
+      distance: 10
+    };
+    options = Object.extend(defaults, options);
+
+    /*
+     * Init data
+     */
+
+    // grid data
+    var distance            = options.distance;
+    var color               = options.color;
+    var i = 0, j = 0;
+
+    // canvas data
+    var canvas = this;
+    var canvas_height       = canvas.height;
+    var canvas_width        = canvas.width;
+    var context             = canvas.getContext("2d");
+    context.strokeStyle     = options.color
+
+    // mouse position data
+    var boundaries = canvas.getBoundingClientRect();
+    var x = 0, y = 0;
+    var info_container_id = options.info_container_id;
+
+    // draw grid
+    var i = 0;
+    for(i = 0; i < canvas_width; i += distance) {
+      context.beginPath();
+      context.moveTo(i + 0.5, 0);
+      context.lineTo(i + 0.5, canvas_height);
+      context.closePath();
+      context.stroke();
+    }
+
+    var j = 0;
+    for (j = 0; j <= canvas_height; j += distance ) {
+      context.beginPath();
+      context.moveTo(0, j + 0.5); 
+      context.lineTo(canvas_width, j + 0.5);
+      context.closePath();
+      context.stroke();
+    }
+
+    // Show cursor coordinates
+    if (info_container_id) {
+      canvas.addEventListener("mousemove", function(e) {
+        x = e.clientX - boundaries.left;
+        y = e.clientY - boundaries.top;
+        message = x + ", " + y;
+        info_container = document.getElementById(info_container_id);
+        info_container.innerHTML = message;
+      });
+    }
+    
+  }
+}
